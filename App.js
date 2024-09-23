7c0336e4b184bfec9240f2e8b4af78af588ac636
Koii <2124801030215@student.tdmu.edu.vn>
2024-09-23 23:30:10 +0700

    Merge branch 'master' of https://github.com/trungkoiiNe/firebaseApp

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import {
+  HomeScreen,
+  ForgotPasswordScreen,
+  LoginScreen,
+  SignupScreen,
+} from "./screens";
+import { RootNavigator } from "./navigation/RootNavigator";
+import { AuthenticatedUserProvider } from "./providers";
+const App = () => {
+  return (
+    <AuthenticatedUserProvider>
+      <SafeAreaProvider>
+        <RootNavigator />
+      </SafeAreaProvider>
+    </AuthenticatedUserProvider>
+  );
+};
+export default App;
